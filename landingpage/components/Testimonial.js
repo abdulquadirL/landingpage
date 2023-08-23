0073d44bbf792bdbf4c45a6957c242c6480e8851
buharry <elanquad92@gmail.com>
2023-08-23 07:00:35 +0100

    add testimonial

--- landingpage/components/Testimonial.js
+++ landingpage/components/Testimonial.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import styles from '@/styles/page.module.css'
+import { Card } from './Card'
 
 const Testimonial = () => {
   return (
-    <div className={styles.container}>
+    <div >
         <div className={styles.company}>
             <h2>What Users are saying about Amplication</h2>
-        <button className={styles.btn2}>Show all testimonials</button>
         </div                  >
-        
+        <div className={styles.grid}>
+            <div><Card/></div>
+            <div><Card/></div>
+        </div>
+        <div className={styles.button}><button className={styles.btn2}>Show all testimonials</button></div>
     </div>
+    
   )
 }
 
