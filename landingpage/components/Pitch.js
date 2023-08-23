aaf931cf155be677e6dcf7964b8faaafaa4c5eb8
buharry <elanquad92@gmail.com>
2023-08-23 07:03:11 +0100

    add pitch

---
+++ landingpage/components/Pitch.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import styles from '@/styles/page.module.css'
+
+const Pitch = () => {
+  return (
+    <>
+    <div className={styles.gridpitch} >
+        <div>
+            <h2>20X Faster</h2>
+            <p></p>
+        </div>
+        <div>
+            <h2>2 Months</h2>
+            <p></p>
+        </div>
+        <div>
+            <h2>100+ hrs</h2>
+            <p></p>
+        </div>
+        <div>
+            <h2>30%</h2>
+        </div>
+    </div>
+    <div className={styles.gridpitch}>
+        <h2>50+ hrs</h2>
+        <p></p>
+    </div>
+    </>
+  )
+}
+
+export default Pitch
