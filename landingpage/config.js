28074275799611a46bb6b89e4707f4d41729ebd7
buharry <elanquad92@gmail.com>
2023-08-24 22:35:42 +0100

    add config file

---
+++ landingpage/config.js
@@ -0,0 +1,21 @@
+const myConfig = [
+   
+    {
+        photo: "image",
+        name: "Abdul Quadir",
+        company: "Quanut",
+        rating: "rating: 5.0",
+        comment: "There has never been a better time than now to exit the humdrum life and transit into the real humdinger with Amplication. We have not just save time and money since we port to Amplication Backend services but life has been easier"
+    },
+    {
+        photo: "image",
+        name: "Antonio",
+        company: "Anduros",
+        rating: "rating: 4.9",
+        comment: "Using Amplication for our Backend services is the best choice we made so far, we have no regrets"
+    }
+          
+    
+]
+
+export default myConfig
