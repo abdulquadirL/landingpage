344412e16fea102a90e344d1bb79b72200032068
buharry <elanquad92@gmail.com>
2023-08-26 04:09:49 +0100

    add what file

---
+++ landingpage/components/What.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import styles from '@/styles/page.module.css'
+
+export const What = () => {
+  return (
+    <div className={styles.main}>
+        <h4>What do you get from Amplication ?</h4>
+        <div className={styles.flexc}>
+            <div className={styles.flex}>
+                <img src='' alt=''/>
+                <p className={styles.description1}>bhgfjkjfkfkfh</p>
+            </div>
+            <div className={styles.flex}>
+                <img src='' alt=''/>
+                <p className={styles.description1}>bhgfjkjfkfkfh</p>
+            </div>
+            <div className={styles.flex}>
+                <img src='' alt=''/>
+                <p className={styles.description1}>bhgfjkjfkfkfh</p>
+            </div>
+            <div className={styles.flex}>
+                <img src='' alt=''/>
+                <p className={styles.description1}>bhgfjkjfkfkfh</p>
+            </div>
+            <div className={styles.flex}>
+                <img src='' alt=''/>
+                <p className={styles.description1}>bhgfjkjfkfkfh</p>
+            </div>
+            <div className={styles.flex}>
+                <img src='' alt=''/>
+                <p className={styles.description1}>bhgfjkjfkfkfh</p>
+            </div>
+            <div className={styles.flex}>
+                <img src='' alt=''/>
+                <p>bhgfjkjfkfkfh</p>
+            </div>
+        </div>
+    </div>
+  )
+}
